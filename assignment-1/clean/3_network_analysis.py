daf5efd8bad43da8e142ae89d72f8a86931b719b
Leul Wujira <meraf000@gmail.com>
2023-04-07 23:55:56 +0300

    Add centrality summary

---
+++ assignment-1/clean/3_network_analysis.py
@@ -0,0 +1,42 @@
+import os
+import utils
+from graph import *
+from collections import defaultdict
+
+
+def compute_centralities(graph):
+    centrality_algorithms = (
+        degree_centrality,
+        closeness_centrality,
+        betweeness_centrality,
+        eigenvector_centrality,
+        katz_centrality
+    )
+
+    centralitites = {}
+
+    for alg in centrality_algorithms:
+        centralitites[alg] = alg(graph)
+
+    return centralitites
+
+
+romania_file = os.path.join(os.path.dirname(
+    __file__), "romania-data/romania-road-distance.txt")
+
+romania_map = utils.load_city_graph(romania_file)
+
+centralities = compute_centralities(romania_map)
+
+with open("output.csv", "w") as f:
+    f.write("Algorithm,")
+    f.write(",".join([city for city in romania_map.nodes]))
+    f.write("\n")
+
+    for alg in centralities:
+        f.write(alg.__name__)
+        f.write(",")
+
+        f.write(",".join(["%.4f" % centralities[alg][city]
+                for city in romania_map.nodes]))
+        f.write("\n")
